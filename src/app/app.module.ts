179be7c7cb0e49892288d024d703ffce8ade9a68
didin jamaludin <didinj@gmail.com>
2017-01-19 06:56:36 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+import { TabsPage } from '../pages/tabs/tabs';
+import { Page3Page } from '../pages/page3/page3';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Page1,
+    Page2,
+    TabsPage,
+    Page3Page
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Page1,
+    Page2,
+    TabsPage,
+    Page3Page
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
